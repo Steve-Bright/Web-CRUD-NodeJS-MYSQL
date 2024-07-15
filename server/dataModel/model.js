add19552f179c04e210d1e60caff1cedab625d6b
Steve <swemhtet.net@gmail.com>
2024-07-15 18:32:03 +0630

    added another methods

--- server/dataModel/model.js
+++ server/dataModel/model.js
@@ -35,15 +35,36 @@ Employee.delete = (employeeId, result) => {
 
 //function still needed
 Employee.search = (employeeId, result) => {
-    mysql.query("SELECT * FROM employees where id = ?", employeeId, (err, res) => {
+    mysql.query(`SELECT * FROM employees WHERE id = ?`, employeeId, (err, res) => {
         if(err){
             console.log("error: " + err);
             result(err, null)
             return;
         }
 
-        console.log("Found the employee ", {id: resinsertId, ...employeeId})
-        result(null, {id: resinsertId, ...employeeId})
+        console.log("Searched the employee ", res[0])
+        result(null, res[0])
+
+        // if (res.length) {
+        //     console.log("found tutorial: ", res[0]);
+        //     result(null, res[0]);
+        //     return;
+        //   }
+
+        //   result({ kind: "not_found" }, null);
+    })
+}
+
+Employee.update = (currentEmployee, result) => {
+    mysql.query("UPDATE employees SET ? ", currentEmployee, (err, res) => {
+        if(err){
+            console.log(err)
+            result(err, null)
+            return
+        }
+
+        console.log("Updated the employee: " + {id: res.insertId, ...employeeId})
+        result(null, {id: res.insertId, ...employeeId})
     })
 }
 
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const path = require("path")
 const employees = require("./dataModel/model.js")
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
+const { emit } = require("process")
 
 
 const app = express();
@@ -12,13 +13,14 @@ const server = createServer(app);
 const io = new Server(server)
 
 
-
+app.set("view engine", "ejs")
 app.use(express.static(path.join(__dirname, "..", "views", "clientScripts")))
 app.use(methodOverride('_method'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => 
-    res.sendFile("index.html", {root: path.join(__dirname, "..", "views")})
+    // res.sendFile("index.html", {root: path.join(__dirname, "..", "views")})
+    res.render("index")
 )
 
 app.post("/formSubmission", (req, res, next) => {
@@ -34,6 +36,49 @@ app.post("/formSubmission", (req, res, next) => {
         if(err){console.log(err)}
         else{console.log("new employee created: " + data)}
     })
+    next()
+})
+
+app.get("/employeeSearch", (req, res) => {
+    console.log(req.query.employeeId)
+    // res.sendFile("testing.html", {root: path.join(__dirname, "..", "views")})
+    employees.search(req.query.employeeId, (err, data) => {
+        if(err){console.log(err)}
+        else{console.log("Employee: " + data.id + " is found")}
+        // res.sendFile("testing.html", {root: path.join(__dirname, "..", "views")})
+        res.render("testing", {
+            employeeId: data.id,
+            employeeGmail: data.email,
+            employeeName: data.employee_name,
+            employeeAge: data.age,
+            employeeBirthDay: JSON.stringify(data.birthday)
+        })
+        console.log(JSON.stringify(data.birthday))
+    })
+    
+    // next()
+    
+})
+
+app.put("/employeeUpdate", (req, res) => {
+    console.log("this is clicked - employeeUpdate")
+    const employee = {
+        id: req.body.employeeId,
+        email: req.body.employeeGmail,
+        employee_name: req.body.employeeName,
+        age: req.body.employeeAge,
+        birthday: req.body.employeeBirthDay
+    }
+    employees.update(employee, (err, data) => {
+        if(err){
+            console.log(err)
+        }
+        else{
+            console.log(data)
+        }
+    })
+       
+    // res.send("Updated Successfuly")
     next()
 })
 
